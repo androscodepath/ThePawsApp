de6f3bb63bbf49ec3536d8fb72418c729e85c6c0
Andros Slowley <andros@codepath.org>
2025-04-02 17:49:24 -0400

    Layout User Profile

--- ThePawsApp/PawsUserProfileViewController.swift
+++ ThePawsApp/PawsUserProfileViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class PawsUserProfileViewController: UIViewController {
 
+    @IBOutlet weak var selfImageView: UIImageView!
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        selfImageView.layer.cornerRadius = selfImageView.frame.width/2
+        
+        dogImageView.layer.cornerRadius = dogImageView.frame.width/2
     }
     
 
